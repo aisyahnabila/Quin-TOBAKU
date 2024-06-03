07bea05f1dd48ff2e632942f718a5ad5392ed084
aisyahnabila <aisyahnabila2.an@gmail.com>
2024-06-03 16:39:32 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./dist/*.{html,js}'], //ganti path sesuai folder project yang digunakan
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
